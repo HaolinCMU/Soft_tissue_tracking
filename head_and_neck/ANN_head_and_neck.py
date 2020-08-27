3ca92d5f371a5819428bad14bb63d63a43e6c0bc
HaolinCMU <45088193+HaolinCMU@users.noreply.github.com>
2020-08-27 05:41:57 -0400

    Update ANN_head_and_neck.py

--- head_and_neck/ANN_head_and_neck.py
+++ head_and_neck/ANN_head_and_neck.py
@@ -759,6 +759,9 @@ def main():
     figure_folder_path = "figure" # The directory of figure folder. 
     data_file_name = "benchmark20mm1000samples.mat" # The file name of data file. 
     isKCenter = True # True/Flase: Y/N for implementing optimized k-center. 
+    
+    if not os.path.isdir(ANN_folder_path): os.mkdir(ANN_folder_path)
+    if not os.path.isdir(figure_folder_path): os.mkdir(figure_folder_path)
 
 
     # ********************************** DATA PROCESSING ********************************** #
