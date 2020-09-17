aa736b11655f1b708243855662f7705a983108dc
HaolinCMU <haolinl@andrew.cmu.edu>
2020-09-17 10:18:12 -0400

    Add files via upload

--- ANN_parameterization/ANN_HL_parameterization.py
+++ ANN_parameterization/ANN_HL_parameterization.py
@@ -313,24 +313,34 @@ def matrixShrink(data_matrix, fix_indices_list=[]):
     return data_shrinked, nDOF, non_zero_indices_list
     
 
-def zeroMean(data_matrix):
+def zeroMean(data_matrix, training_ratio, mean_vect_input=[]):
     """
     Shift the origin of new basis coordinate system to mean point of the data. 
 
     Parameters:
     ----------
         data_matrix: 2D Array. 
-            Size: nFeatures x nSamples. 
+            Size: nFeatures x nSamples.
+        training_ratio: float.
+            The ratio of training dataset. 
+        mean_vect_input (optional): 1D List of floats. 
+            The user input of mean vector of training dataset. 
+            Default: [].
 
     Returns:
     ----------
         data_new: 2D Array with the same size as data_matrix. 
             Mean-shifted data. 
-        mean_vect: 1D Array. 
+        mean_vect: 1D Array of float. 
             The mean value of each feature. 
     """
     
-    mean_vect = np.mean(data_matrix, axis=1) # Compute mean along with sample's axis. 
+    if mean_vect_input == []:
+        training_index = int(np.ceil(data_matrix.shape[1] * training_ratio)) # Samples along with axis-1.
+        mean_vect = np.mean(data_matrix[:,0:training_index], axis=1) # Compute mean along with sample's axis. 
+    else:
+        mean_vect = np.array(mean_vect_input).astype(float).reshape(-1,)
+
     data_new = np.zeros(data_matrix.shape)
 
     for i in range(data_matrix.shape[1]):
@@ -945,12 +955,12 @@ def main():
     # Extract data from .mat file
     data_mat = scipy.io.loadmat("benchmark20mm1000samples.mat")
     v_space, data_x, fix_dof_list = data_mat["NodeI"], data_mat["xI"], data_mat["idxFix3"] # change the variable's name if necessary. 
-    fix_node_list = [ind for ind in fix_dof_list if ind % 3 == 0] # Indices of fixed nodes. Indexed from 1. 
+    fix_node_list = [int(ind/3) for ind in fix_dof_list if ind % 3 == 0] # Indices of fixed nodes. Indexed from 1. 
 
     # Implement PCA
     orig_node_num = int(data_x.shape[0] / 3.0)
     data_x, nDOF, non_zero_indices_list = matrixShrink(data_x) # Remove zero rows of data_x.
-    data_x, mean_vect = zeroMean(data_x) # Shift(zero) the data to its mean
+    data_x, mean_vect = zeroMean(data_x, training_ratio) # Shift(zero) the data to its mean
     eigVect_full, eigVal_full, eigVect, eigVal, data_y = PCA(data_x, PC_num, training_ratio) # PCA on training deformation matrix. 
 
     # Generate FM indices
